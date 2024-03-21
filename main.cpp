4e1b0f33c6a2366d613d95ed4500db34b1335c30
Matthew Allen <matthew.allen@soundunited.com>
2024-03-22 08:12:51 +1100

    Windows support.

--- main.cpp
+++ main.cpp
@@ -18,6 +18,15 @@
 #include <smb2/libsmb2.h>
 #include <smb2/libsmb2-raw.h>
 
+#define CODE_REF		code_ref(__FILE__, __LINE__, __func__).c_str()
+extern std::string code_ref( const char *file, int line, const char *func );
+
+#define LOG_DEBUG(...)	log_printf("DEBG", __FILE__, __LINE__, __func__, __VA_ARGS__)
+#define LOG_INFO(...)	log_printf("INFO", __FILE__, __LINE__, __func__, __VA_ARGS__)
+#define LOG_WARN(...)	log_printf("WARN", __FILE__, __LINE__, __func__, __VA_ARGS__)
+#define LOG_ERR(...)	log_printf("ERRR", __FILE__, __LINE__, __func__, __VA_ARGS__)
+extern void log_printf(const char *type, const char *file, int line, const char *func, ...);
+
 /*
  * Command line options
  *
@@ -33,20 +42,40 @@ static struct options {
 static smb2_context *smb2 = nullptr;
 std::string smb2_path;
 
-#define DEFAULT_USER_ID     1000
-#define DEFAULT_GROUP_ID    1000
 
-// Files are 644
-#define SMB_FILE_READ       S_IRUSR | \
-                            S_IRGRP | \
-                            S_IROTH
-#define SMB_FILE_WRITE      S_IWUSR
+#ifdef _WINDOWS
+	
+	#define DEFAULT_USER_ID     0
+	#define DEFAULT_GROUP_ID    0
+	
+	#define SMB_FILE			_S_IFREG
+	#define SMB_LINK			_S_IFREG
+	#define SMB_FILE_READ		_S_IREAD
+	#define SMB_FILE_WRITE		_S_IWRITE
+	#define SMB_DIR				_S_IFDIR
+	#define SMB_DIR_READ		_S_IREAD | _S_IEXEC | 0x4 /* IDK what this is... but windows needs it */
+	#define SMB_DIR_WRITE		_S_IWRITE
 
-// Dir are 755
-#define SMB_DIR_READ        S_IRUSR | S_IXUSR | \
-                            S_IRGRP | S_IXGRP | \
-                            S_IROTH | S_IXOTH
-#define SMB_DIR_WRITE       S_IWUSR
+#else
+	
+	#define DEFAULT_USER_ID     1000
+	#define DEFAULT_GROUP_ID    1000
+
+	// Files are 644
+	#define SMB_FILE			S_IFREG
+	#define SMB_LINK			S_IFLNK
+	#define SMB_FILE_READ       S_IRUSR | \
+								S_IRGRP | \
+								S_IROTH
+	#define SMB_FILE_WRITE      S_IWUSR
+
+	// Dir are 755
+	#define SMB_DIR				S_IFDIR
+	#define SMB_DIR_READ        S_IRUSR | S_IXUSR | \
+								S_IRGRP | S_IXGRP | \
+								S_IROTH | S_IXOTH
+	#define SMB_DIR_WRITE       S_IWUSR
+#endif
 
 #define OPTION(t, p)                           \
     { t, offsetof(struct options, p), 1 }
@@ -65,13 +94,25 @@ static std::string full_path(const char *path)
     return p;
 }
 
-#define CODE_REF code_ref(__FILE__, __LINE__, __func__).c_str()
 static std::string code_ref( const char *file, int line, const char *func )
 {
     char s[256];
     auto last = strrchr(file, '/');
     snprintf(s, sizeof(s), "%s:%i:%s", last?last+1:file, line, func);
     return s;
+}
+
+void log_printf(const char *type, const char *file, int line, const char *func, const char *fmt, ...)
+{
+	char s[512];
+	auto ch = snprintf(s, sizeof(s), "%s:%s:%i:%s ", type, file, line, func);
+
+	va_list args;
+	va_start(args, fmt);
+	vsnprintf(s+ch, sizeof(s)-ch, fmt, args);
+	va_end(args);
+
+	printf("%s\n", s);
 }
 
 static void *wrapper_init(struct fuse_conn_info *conn,
@@ -82,29 +123,33 @@ static void *wrapper_init(struct fuse_conn_info *conn,
     return NULL;
 }
 
-static bool convert_stat(struct stat *out, smb2_stat_64 *in)
+static bool convert_stat(struct fuse_stat *out, smb2_stat_64 *in)
 {
-    out->st_size = in->smb2_size;
+    out->st_size = (off_t)in->smb2_size;
     out->st_mtim.tv_sec = (time_t)in->smb2_mtime;
     out->st_atim.tv_sec = (time_t)in->smb2_atime;
     out->st_ctim.tv_sec = (time_t)in->smb2_ctime;
     
+	#if 0
     bool read_only = (in->smb2_attrib & SMB2_FILE_ATTRIBUTE_READONLY) != 0;
+	#else
+	bool read_only = false;
+	#endif
 
     switch (in->smb2_type)
     {
     case SMB2_TYPE_LINK:
-        out->st_mode = S_IFLNK | SMB_FILE_READ;
+        out->st_mode = SMB_LINK | SMB_FILE_READ;
         if (!read_only)
             out->st_mode |= SMB_FILE_WRITE;
         break;
     case SMB2_TYPE_FILE:
-        out->st_mode = S_IFREG | SMB_FILE_READ;
+        out->st_mode = SMB_FILE | SMB_FILE_READ;
         if (!read_only)
             out->st_mode |= SMB_FILE_WRITE;
         break;
     case SMB2_TYPE_DIRECTORY:
-        out->st_mode = S_IFDIR | SMB_DIR_READ;
+        out->st_mode = SMB_DIR | SMB_DIR_READ;
         if (!read_only)
             out->st_mode |= SMB_DIR_WRITE;
         break;
@@ -117,18 +162,20 @@ static bool convert_stat(struct stat *out, smb2_stat_64 *in)
 }
 
 static int wrapper_getattr( const char *path,
-                            struct stat *stbuf,
+                            struct fuse_stat *stbuf,
                             struct fuse_file_info *fi )
 {
     (void) fi;
 
+	LOG_DEBUG("path=%s", path);
+
     auto full = full_path( path );
     memset(stbuf, 0, sizeof(struct stat));
 
-    printf("%s getattr(%s)\n", CODE_REF, full.c_str());
     if( strcmp(path, "/") == 0 ) 
     {
-        stbuf->st_mode = S_IFDIR | SMB_DIR_READ | SMB_DIR_WRITE;
+		// 0x41ff
+        stbuf->st_mode = SMB_DIR | SMB_DIR_READ | SMB_DIR_WRITE;
         stbuf->st_nlink = 2;
         stbuf->st_uid = DEFAULT_USER_ID;
         stbuf->st_gid = DEFAULT_GROUP_ID;
@@ -140,7 +187,7 @@ static int wrapper_getattr( const char *path,
         if( -EACCES == result )
         {
             // For entries without permissions we create an empty stat record rather than ENOENT
-            stbuf->st_mode = S_IFREG;
+            stbuf->st_mode = SMB_FILE;
             stbuf->st_uid = DEFAULT_USER_ID;
             stbuf->st_gid = DEFAULT_GROUP_ID;
             return 0;
@@ -165,12 +212,14 @@ static int wrapper_getattr( const char *path,
 static int wrapper_readdir( const char *path,
                             void *buf,
                             fuse_fill_dir_t filler,
-                            off_t offset,
+                            fuse_off_t offset,
                             struct fuse_file_info *fi,
                             enum fuse_readdir_flags flags )
 {
     auto none = (fuse_fill_dir_flags)0;
     auto full = full_path( path );
+
+	LOG_DEBUG("path=%s", path);
 
     if (!smb2)
     {
@@ -190,7 +239,7 @@ static int wrapper_readdir( const char *path,
     smb2dirent *ent = nullptr;
     while ((ent = smb2_readdir(smb2, dir)))
     {
-        struct stat st = {};
+        struct fuse_stat st = {};
         convert_stat(&st, &ent->st);
         filler(	buf,
                 ent->name,
@@ -227,6 +276,8 @@ static int wrapper_readdir( const char *path,
 static int wrapper_open( const char *path,
                          struct fuse_file_info *fi)
 {
+	LOG_DEBUG("path=%s", path);
+
     auto full = full_path( path );
 
     smb2fh *fh = smb2_open(smb2, full.c_str(), fi->flags);
@@ -244,9 +295,11 @@ static int wrapper_open( const char *path,
 static int wrapper_read( const char *path,
                          char *buf,
                          size_t size,
-                         off_t offset,
+                         fuse_off_t offset,
                          struct fuse_file_info *fi )
 {
+	LOG_DEBUG("path=%s", path);
+
     if( !fi )
     {
         printf( "%s error: no file info\n", CODE_REF );
@@ -266,7 +319,7 @@ static int wrapper_read( const char *path,
         return -EFAULT;
     }
 
-    printf( "%s smb2_read @ %li (%p, %zi)", CODE_REF, offset, buf, size );
+    printf( "%s smb2_read @ %i (%p, %zi)", CODE_REF, (int)offset, buf, size );
     auto rd = smb2_read( smb2, fh, (uint8_t*) buf, size );
     printf( " = %i\n", rd );
     return rd;
@@ -274,6 +327,8 @@ static int wrapper_read( const char *path,
 
 static int wrapper_release( const char *path, struct fuse_file_info *fi )
 {
+	LOG_DEBUG("path=%s", path);
+
     if( !fi )
     {
         printf( "%s error: no file info\n", CODE_REF );
@@ -313,6 +368,16 @@ int main(int argc, char *argv[])
 {
     int ret;
     struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
+
+	#ifdef _WINDOWS
+	WSADATA _data;
+	WSAStartup(MAKEWORD(2, 2), &_data);
+	#endif
+
+	struct stat st;
+	auto r = stat("C:\\Users\\Matthew\\work\\smb", &st);
+	int asd=0;
+
 
     /* Set defaults -- we have to use strdup so that
        fuse_opt_parse can free the defaults if other
@@ -354,6 +419,7 @@ int main(int argc, char *argv[])
                 url->path);
 
         smb2_set_security_mode(smb2, SMB2_NEGOTIATE_SIGNING_ENABLED);
+		smb2_set_password(smb2, "_groon");
         if (smb2_connect_share(smb2, url->server, url->share, url->user) < 0)
         {
             printf("smb2_connect_share failed. %s\n", smb2_get_error(smb2));
